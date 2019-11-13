f8bf70c621962384a45a6f3c00c6a0b0761bf6ea
Ryan Eckert <reckert2017@fau.edu>
2019-11-13 17:21:39 -0500

    Update renameSeqData.py

--- scripts/renameSeqData.py
+++ scripts/renameSeqData.py
@@ -4,23 +4,15 @@ import os
 import re
 
 path = os.getcwd()
-pattern1 = re.compile(r'S(\d)+_')
-pattern2 = re.compile(r'RE-P(\d)-')
+pattern1 = re.compile(r'S(\d)+_L(\d)+_')
 targets = []
 
-for fileName in [fileName for fileName in os.listdir(path) if fileName.endswith('.fastq')]:
-	os.rename(fileName, fileName.replace('_001',''))
+for fileName in [fileName for fileName in os.listdir(path) if fileName.endswith('.fastq.gz')]:  
+        m1 = pattern1.search(fileName)
+        if m1:
+                os.rename(fileName, fileName.replace(m1.group(0),''))
 
-for fileName in [fileName for fileName in os.listdir(path) if fileName.endswith('.fastq')]:
-	os.rename(fileName, fileName.replace('L001_',''))
+for fileName in [fileName for fileName in os.listdir(path) if fileName.endswith('.fastq.gz')]:
+        os.rename(fileName, fileName.replace('_001.','.'))
 
-for fileName in [fileName for fileName in os.listdir(path) if fileName.endswith('.fastq')]:	
-	m1 = pattern1.search(fileName)
-	if m1:
-		os.rename(fileName, fileName.replace(m1.group(0),''))
-
-for fileName in [fileName for fileName in os.listdir(path) if fileName.endswith('.fastq')]:
-	m2 = pattern2.match(fileName)
-	if m2:
-		os.rename(fileName, fileName.replace(m2.group(0),''))
 print('DONE')
